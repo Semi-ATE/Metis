5d95feb1af06dbf479cf5312afecb9d33acd51fc
seimit <kkos@semi-ate.com>
2022-08-21 16:54:15 +0300

    Fix h5py import error

--- Metis/python/metis_source.py
+++ Metis/python/metis_source.py
@@ -5,11 +5,11 @@ import sys
 with open("/home/runner/work/Metis/Metis/sys_source.path", "w") as f:
     f.write(str(sys.path))
 
-sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/bin")
-sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python310.zip")
-sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python3.10")
-sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python3.10/lib-dynload")
-sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python3.10/site-packages")
+#sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/bin")
+#sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python310.zip")
+#sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python3.10")
+#sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python3.10/lib-dynload")
+#sys.path.append("/opt/hostedtoolcache/Python/3.10.5/x64/lib/python3.10/site-packages")
 
 import os
 import logging
