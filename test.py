84b67b607f1c3e95fbe7c141b8190010126fcc9a
seimit <kkos@semi-ate.com>
2022-08-21 13:23:42 +0300

    Fix h5py import error

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import h5py
 print("==============================")
 print("h5py was successfully imported")
 print("==============================")
+import sys
 
 def test():
-    print("test")
+    print("sys path :")
+    print(sys.path)
